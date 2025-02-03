dcf824a49d61f6496ca627acb1132e1ee7d3593a
Sebastian <103226759+SebastianPoncin@users.noreply.github.com>
2025-02-03 12:50:44 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -43,7 +43,7 @@ public class PalindromeChecker {
   public String reverse(String str)
   {
       String sNew = new String();
-      for(int i = str.length(); i >= 0; i++) {
+      for(int i = str.length()-1; i >= 0; i--) {
         sNew += str.substring(i, i+1);  
       }
       return sNew;
